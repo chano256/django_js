2a709ccae282661af87c20ad65623a863207075b
Allan Ochan <allanochan@yahoo.com>
2023-01-31 22:06:31 +0300

    adds env file for sensitive info

--- django_js/settings.py
+++ django_js/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv, environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,10 +78,10 @@ WSGI_APPLICATION = 'django_js.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_js_db',
-        'USER': 'django_js',
-        'PASSWORD': 'django_js',
-        'HOST': 'localhost',
+        'NAME': getenv('DB_NAME'),
+        'USER': getenv('DB_USER'),
+        'PASSWORD': environ.get('DB_PASSWORD'),
+        'HOST': environ.get('DB_HOST'),
 
         # 'PORT': '5432',
         # 'ENGINE': 'django.db.backends.sqlite3',
