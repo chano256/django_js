f6aeea848855c2cdf83b5876a354d1ac376728fa
Allan Ochan <allanochan@yahoo.com>
2023-02-19 19:39:50 +0300

    customizes admin panel

--- expenses/admin.py
+++ expenses/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Expense, Category
 
 # Register your models here.
-admin.site.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'category', 'amount', 'description')
+    search_fields = ('category', 'description')
+
+admin.site.register(Expense, ExpenseAdmin)
 admin.site.register(Category)
